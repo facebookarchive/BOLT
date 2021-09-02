6ca09f3053f3d8a9b84783576f988f5642f88597
Maksim Panchenko <maks@fb.com>
2021-09-01 21:40:54 -0700

    [BOLT][DWARF] Move line info emission into BOLT
    Summary:
    BOLT needs to generate line info tables using absolute addresses as well
    as using the standard MC way of labels attached to instructions. Move
    line table generation code under BOLT.
    
    Ideally, we should be able to extend existing interfaces in LLVM, but
    without other users of the interface it will be hard to justify the
    change.

--- bolt/src/BinaryContext.cpp
+++ bolt/src/BinaryContext.cpp
@@ -1396,6 +1396,16 @@ void BinaryContext::printGlobalSymbols(raw_ostream& OS) const {
   }
 }
 
+Expected<unsigned>
+BinaryContext::getDwarfFile(StringRef Directory, StringRef FileName,
+                            unsigned FileNumber,
+                            Optional<MD5::MD5Result> Checksum,
+                            Optional<StringRef> Source, unsigned CUID) {
+  DwarfLineTable &Table = DwarfLineTablesCUMap[CUID];
+  return Table.tryGetFile(Directory, FileName, Checksum, Source,
+                          Ctx->getDwarfVersion(), FileNumber);
+}
+
 unsigned BinaryContext::addDebugFilenameToUnit(const uint32_t DestCUID,
                                                const uint32_t SrcCUID,
                                                unsigned FileIndex) {
@@ -1422,7 +1432,7 @@ unsigned BinaryContext::addDebugFilenameToUnit(const uint32_t DestCUID,
           FileNames[FileIndex - 1].Name.getAsCString())
     FileName = *FName;
   assert(FileName != "");
-  return cantFail(Ctx->getDwarfFile(Dir, FileName, 0, None, None, DestCUID));
+  return cantFail(getDwarfFile(Dir, FileName, 0, None, None, DestCUID));
 }
 
 std::vector<BinaryFunction *> BinaryContext::getSortedFunctions() {
@@ -1546,12 +1556,12 @@ void BinaryContext::preprocessDebugInfo() {
         LineTable->Prologue.FileNames;
 
     // Assign a unique label to every line table, one per CU.
-    Ctx->getMCDwarfLineTable(CUID).setLabel(
-      Ctx->getOrCreateSymbol(GlobalPrefix + "line_table_start" + Twine(CUID)));
+    getDwarfLineTable(CUID).setLabel(Ctx->getOrCreateSymbol(
+        GlobalPrefix + "line_table_start" + Twine(CUID)));
 
     // Make sure empty debug line tables are registered too.
     if (FileNames.empty()) {
-      cantFail(Ctx->getDwarfFile("", "<unknown>", 0, None, None, CUID));
+      cantFail(getDwarfFile("", "<unknown>", 0, None, None, CUID));
       continue;
     }
     for (size_t I = 0, Size = FileNames.size(); I != Size; ++I) {
@@ -1567,7 +1577,7 @@ void BinaryContext::preprocessDebugInfo() {
       if (Optional<const char *> FName = FileNames[I].Name.getAsCString())
         FileName = *FName;
       assert(FileName != "");
-      cantFail(Ctx->getDwarfFile(Dir, FileName, 0, None, None, CUID));
+      cantFail(getDwarfFile(Dir, FileName, 0, None, None, CUID));
     }
   }
 
