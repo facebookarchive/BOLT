b8fd04e79f7ae2e688d83923f208ec41c472beec
Vladislav Khmelevsky <och95@yandex.ru>
2021-10-16 17:35:29 +0300

    Disable instrumentation and hugify build for aarch64
    This patch temporarily disables instrumentation and higufy build not for
    x86 platforms to be able to build llvm-bolt tool on aarch64.

--- bolt/runtime/instr.cpp
+++ bolt/runtime/instr.cpp
@@ -40,6 +40,7 @@
 //
 //===----------------------------------------------------------------------===//
 
+#if defined (__x86_64__)
 #include "common.h"
 
 // Enables a very verbose logging to stderr useful when debugging
@@ -1668,4 +1669,5 @@ void _bolt_instr_fini() {
   __bolt_instr_data_dump();
 }
 
+#endif
 #endif
