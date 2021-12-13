MatzeB/llvm-project

Rebase: Merge BOLT codebase in monorepo
Summary:
This commit is the first step in rebasing all of BOLT
history in the LLVM monorepo. It also solves trivial build issues
by updating BOLT codebase to use current LLVM. There is still work
left in rebasing some BOLT features and in making sure everything
is working as intended.

History has been rewritten to put BOLT in the /bolt folder, as
opposed to /tools/llvm-bolt.

Manual rebase conflict history:
https://phabricator.intern.facebook.com/D28072883

Test Plan:
built with -DLLVM_ENABLE_PROJECTS="clang;lld;bolt" and
-DLLVM_TARGETS_TO_BUILD="X86" and verified build finishes.

Reviewers: #compiler_toolchain

Differential Revision: https://phabricator.intern.facebook.com/D28112747

Tasks: T89807543

bolt/src/YAMLProfileWriter.cpp
@@ -151,7 +151,7 @@ YAMLProfileWriter::writeProfile(const RewriteInstance &RI) {
   const auto &Functions = BC.getBinaryFunctions();
 
   std::error_code EC;
-  OS = llvm::make_unique<raw_fd_ostream>(Filename, EC, sys::fs::F_None);
+  OS = std::make_unique<raw_fd_ostream>(Filename, EC, sys::fs::F_None);
   if (EC) {
     errs() << "BOLT-WARNING: " << EC.message() << " : unable to open "
            << Filename << " for output.\n";
@@ -162,10 +162,10 @@ YAMLProfileWriter::writeProfile(const RewriteInstance &RI) {
 
   // Fill out the header info.
   BP.Header.Version = 1;
-  BP.Header.FileName = BC.getFilename();
+  BP.Header.FileName = std::string(BC.getFilename());
   auto BuildID = BC.getFileBuildID();
-  BP.Header.Id = BuildID ? *BuildID : "<unknown>";
-  BP.Header.Origin = RI.getProfileReader()->getReaderName();
+  BP.Header.Id = BuildID ? std::string(*BuildID) : "<unknown>";
+  BP.Header.Origin = std::string(RI.getProfileReader()->getReaderName());
 
   auto EventNames = RI.getProfileReader()->getEventNames();
   if (!EventNames.empty()) {

facebookarchive/BOLT

(cherry picked from FBD32739051)

bolt/src/YAMLProfileWriter.cpp
@@ -151,7 +151,7 @@ YAMLProfileWriter::writeProfile(const RewriteInstance &RI) {
   const auto &Functions = BC.getBinaryFunctions();
 
   std::error_code EC;
-  OS = llvm::make_unique<raw_fd_ostream>(Filename, EC, sys::fs::F_None);
+  OS = std::make_unique<raw_fd_ostream>(Filename, EC, sys::fs::OF_None);
   if (EC) {
     errs() << "BOLT-WARNING: " << EC.message() << " : unable to open "
            << Filename << " for output.\n";
@@ -162,10 +162,10 @@ YAMLProfileWriter::writeProfile(const RewriteInstance &RI) {
 
   // Fill out the header info.
   BP.Header.Version = 1;
-  BP.Header.FileName = BC.getFilename();
+  BP.Header.FileName = std::string(BC.getFilename());
   auto BuildID = BC.getFileBuildID();
-  BP.Header.Id = BuildID ? *BuildID : "<unknown>";
-  BP.Header.Origin = RI.getProfileReader()->getReaderName();
+  BP.Header.Id = BuildID ? std::string(*BuildID) : "<unknown>";
+  BP.Header.Origin = std::string(RI.getProfileReader()->getReaderName());
 
   auto EventNames = RI.getProfileReader()->getEventNames();
   if (!EventNames.empty()) {